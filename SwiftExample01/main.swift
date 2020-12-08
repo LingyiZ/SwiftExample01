0a679616333f90b251c46c6b4b4ca67553d08d04
Lingyi Zhan <shinycatherine01@gmail.com>
2020-12-08 00:07:27 -0600

    First example to try

---
+++ SwiftExample01/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  SwiftExample01
+//
+//  Created by Lingyi Zhan on 11/4/20.
+//  Copyright © 2020 Lingyi Zhan. All rights reserved.
+//
+
+import Foundation
+
+var name = "Lingyi"
+var age = 25
+
+name = "Xiaoxiao"
+age = 19
+
+print(name, age)
