a0edb31e0ea604003c4a6f0fdb341cdeea53ca6d
Lingyi Zhan <shinycatherine01@gmail.com>
2020-12-20 19:37:56 -0600

    Update main.swift

--- SwiftExample01/main.swift
+++ SwiftExample01/main.swift
@@ -15,3 +15,29 @@ name = "Xiaoxiao"
 age = 19
 
 print(name, age)
+
+
+class Location {
+var latitude : Double
+var longitude : Double
+var altitude : Double
+var test : Double?
+
+    init(latitude: Double, longitude: Double, altitude: Double) {
+    
+        self.latitude = latitude
+        self.longitude = longitude
+        self.altitude = altitude
+    }
+}
+
+
+let position = Location(latitude: 38.9473, longitude: -92.3287, altitude: 758.2)
+
+let objectType = type(of: position)
+
+print("Object type: \(objectType)")
+
+
+let objectType11 = type(of: position.test)
+print("Object type 11: \(objectType)")
